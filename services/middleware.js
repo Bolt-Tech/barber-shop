3b3c05b9e9bdc27db530a73fe86a075da287ee95
Bolt-Tech <68707389+Bolt-Tech@users.noreply.github.com>
2021-10-13 16:34:14 +0300

    Merged login for customer & admin, organized functions & data list

--- services/middleware.js
+++ services/middleware.js
@@ -1,11 +1,12 @@
 import jwt from 'jsonwebtoken';
-
+//Changed email to name because in JSON log was written name: "<customer email>" line 8 and 9
 export const authenticateToken = (req, res, next) => {
     const token = req.header("x-api-token")
     try {
         const decoded = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
-        req.authenticateMessage = `Welcome ${decoded.email}, you are authenticated`
-        req.userEmail = decoded.email;
+        console.log("(middleware)decoded=>"+JSON.stringify(decoded));
+        req.authenticateMessage = `Welcome ${decoded.name}, you are authenticated`
+        req.userEmail = decoded.name;
         next()
       } catch(error) {
         console.log(error)
--- services/queries.js
+++ services/queries.js
@@ -1,7 +1,43 @@
-import e from 'express';
 import { connection } from './db_access.js';
 import { generateHash, checkPassword } from './encryption.js'
 import jwt from 'jsonwebtoken';
+
+export const getToken = async(req, res) => {
+    try {
+        const token = jwt.sign({ name: "Muli" }, 'shhhhh');
+        res.send(token);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//This function used to generate table data appointments list and customer names for admin to read.
+export const getAllAppointments = async (res) => {    
+    const [rows] = await connection.query(`SELECT time, concat(first_name,' ', last_name) as fullname FROM appointments a INNER JOIN customers c ON c.id=a.customer_id WHERE time IS NOT NULL`);
+    if (rows[0]) {
+        let htmlTable = `<table>
+        <thead>
+        <tr>
+        <th>Full name</th>
+        <th>Time</th>
+        </tr>
+        </thead>`;
+        rows.forEach(row => {
+            htmlTable += `<tr>
+            <td>${row.fullname}</td> 
+            <td>${row.time}</td>
+            </tr>`;
+        });
+        htmlTable += `</table>`;
+        //console.log(htmlTable); //Prints a long JSON codes
+        res.send(htmlTable);
+    }
+    else {
+        console.log("Appointments are empty");
+        res.send('<h1>ApAppointments are empty!</h1>')
+        return;
+    }
+}
 
 export const getAppointment = async (hour) => {    
     const [rows] = await connection.query(`SELECT * FROM appointments WHERE time='${hour}'`);
@@ -18,11 +54,26 @@ export const getAppointment = async (hour) => {
     }
 }
 
-export const setAppointment = async(appointmentData) => {
-    const { selectedTime, phone } = appointmentData;
+export const setAppointment = async(appointmentData, token) => {
+    //const { selectedTime, phone } = appointmentData;
+    const { selectedTime } = appointmentData;
     const appointmentAvailable = await getAppointment(selectedTime);
+    let userEmail = "";
     if(appointmentAvailable) {
-        const customers = await getCustomer(phone);
+        try {
+            const decodedToken = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+            //console.log("(queries)decoded? " + JSON.stringify(decodedToken));
+
+            userEmail = decodedToken.name;
+        }
+        catch(error) {
+            console.log("Token error details: " + error);
+            return;
+        }
+
+        const customers = await getCustomer(userEmail);
+        //console.log("email? " + userEmail);
+        //console.log("token? " + token);
         if (customers[0]) {
             const { id: customerId } = customers[0]
             await connection.query(`UPDATE appointments SET available = 0, customer_id = '${parseInt(customerId)}' WHERE time='${selectedTime}'`);
@@ -34,6 +85,16 @@ export const setAppointment = async(appointmentData) => {
     }
 }
 
+export const resetAppointments = async() => {
+    try {
+        console.log('All appointments are now available!');
+        await connection.query(`UPDATE appointments SET available=1, customer_id=NULL`);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//A function to create customer account
 export const createCustomer = async(req) => {
     const { firstName, lastName, email, phone, password } = req.body;
     const hashedPassword = await generateHash(password);
@@ -51,40 +112,47 @@ export const createCustomer = async(req) => {
     }
 }
 
-const getCustomer = async(phone) => {
+const getCustomer = async(email) => {
     try {
-        const [customers] = await connection.query(`SELECT * FROM customers WHERE phone='${phone}'`);
+        const [customers] = await connection.query(`SELECT * FROM customers WHERE email='${email}'`);
         return customers;
     } catch (error){
         console.log(error);
     }
 }
 
-export const resetAppointments = async() => {
+
+//* Login for customers making appointment, while admins read taken appointments with each customer names
+export const login = async (req, res) => {
     try {
-        console.log('All appointments are now available!');
-        await connection.query(`UPDATE appointments SET available=1, customer_id=NULL`);
+        const { email, password } = req.body;
+        let userTypeValue = 'customer';
+        let [rows] = await connection.query(`SELECT password FROM admins WHERE email='${email}'`);
+        if (rows[0]) {
+            userTypeValue = 'admin';
+        } else {
+            [rows] = await connection.query(`SELECT password FROM customers WHERE email='${email}'`);
+        }
+        if (rows[0]) {
+            const hashedPassword = rows[0].password;
+            const verfiedPassword = await checkPassword(password, hashedPassword);
+            console.log("Is password verfied?\t" + verfiedPassword); //? Debugging for making sure password is working
+
+            if (verfiedPassword) {
+                const { JWT_TOKEN_SECRET } = process.env;
+                //console.log("User's password:\t" + JWT_TOKEN_SECRET); //? Debug only to test if password is received
+                const token = jwt.sign({ name: email }, JWT_TOKEN_SECRET, { expiresIn: '5h' });
+                //console.log("Token Key:\t\t" + token); //? Debug only to test if token is received
+                res.status(200).json({ accessToken: token, userType: userTypeValue });
+            }
+            else {
+                res.status(404).send('Email or password are incorrect, please try again.');
+            }
+        }
+        else {
+            res.status(404).send('Email or password are incorrect, please try again.');
+        }
     } catch (error) {
         console.log(error)
-    }
-}
-
-export const login = async(req, res) => {
-    const { email, password } = req.body;
-    const [rows] = await connection.query(`SELECT password FROM customers WHERE email='${email}'`);
-    if (rows[0]) {
-        const hashedPassword = rows[0].password;
-        const verfiedPassword = await checkPassword(password, hashedPassword);
-        console.log(verfiedPassword)
-        if (verfiedPassword) {
-            const token = jwt.sign({ name: email }, process.env.JWT_TOKEN_SECRET, { expiresIn: '5h' });
-            res.status(200).json({accessToken: token});       
-            }
-        else {
-            res.status(404).send('Username or password are incorrect');
-        }
-    }
-    else {
-        res.status(404).send('Username or password are incorrect');
     }
 }
