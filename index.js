817e0e6fb6e0a321fca8d7aec3c5ea27761235ba
Bolt-Tech <68707389+Bolt-Tech@users.noreply.github.com>
2021-10-13 16:35:43 +0300

    Code cleanup, adde link for admin login, fixed token appointment

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import cron from 'node-cron';
-import { getAppointment, resetAppointments, createCustomer, login, setAppointment } from './services/queries.js'
+import { resetAppointments, createCustomer, login, setAppointment, getToken, getAllAppointments } from './services/queries.js'
 import { authenticateToken } from './services/middleware.js';
-import jwt from 'jsonwebtoken';
 import cors from 'cors';
 const app = express();
 const port = 3000;
@@ -26,15 +25,33 @@ app.post('/signUp', async (req, res) => {
   createCustomer(req);
   // res.send()
 })
+//TODO: Reorganize HTML table queries data API (Part B + C)
+//Part 2, B - Admin login
+app.get('/admin-login', async (req, res) => {
+  await getAllAppointments(res);
+  
+  //const { customerName, appointmentTime} = req.body;
+//console.log(res);
+//console.log(req.body);
+})
 
 app.post('/login', async (req, res) => {
   await login(req, res);
 })
 
 app.post('/set-appointment', authenticateToken, async (req, res) => {
-  console.log(req.body);
-  res.send(req.authenticateMessage)
-  await setAppointment(req.body);
+  let token = req.headers["x-api-token"];
+  //let token2 = req.headers.x-api-token[7];//unsure
+  //console.log("Header request: " + JSON.stringify(req.headers.x-api-token)); //? Debug
+  console.log("Header request token: " + JSON.stringify(token)); //? Debug
+  //console.log("Header request token2: " + JSON.stringify(token2)); //? Debug
+  console.log(JSON.stringify(req.body));
+  res.send(req.authenticateMessage);
+  await setAppointment(req.body, token);
+})
+
+app.get('/get-token', async (req, res) => { //? Temporary quick check token
+  await getToken(req,res);
 })
 
 app.listen(port, () => {
